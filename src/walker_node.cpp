f1d11340d99346c06a6feed41e47758b366fa8f4
Dani Lerner <dlerner97@gmail.com>
2021-11-23 13:33:54 -0500

    Error from a few commits ago had to do with simulation time. Fixed it by adding sleep and waiting for sim time to spin up

--- src/walker_node.cpp
+++ src/walker_node.cpp
@@ -7,5 +7,9 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "walker_node");
     ros::NodeHandle nh;
     SimpleWalker walker(&nh);
+    ros::Duration(10).sleep();
+    while (ros::Time::now().is_zero()) ros::Duration(0.5).sleep();
+    walker.walk(1, 1);
+
 
 }
