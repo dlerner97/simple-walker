b57434fab863687126b7479ea101d65f06fc1eeb
Dani Lerner <dlerner97@gmail.com>
2021-11-29 09:48:37 -0500

    Changed distance from obj param

--- src/danger_node.cpp
+++ src/danger_node.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "danger_node");
     ros::NodeHandle nh;
     ros::Duration(5).sleep();
-    DistanceAlerter alerter(&nh, 2);
+    DistanceAlerter alerter(&nh, 1);
     ros::spin();
     return 0;
 }
