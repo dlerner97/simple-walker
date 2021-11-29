6a1203118ccb340bd35aaf951a0933da1e317d7b
Dani Lerner <dlerner97@gmail.com>
2021-11-28 20:04:12 -0500

    This class subscribes to the the scan topic and publishes to the danger topic if an object is too close

--- include/distance_alerter.hpp
+++ include/distance_alerter.hpp
@@ -1,24 +1,34 @@
+/**
+ * @file distance_alerter.hpp
+ * @author Dani Lerner (dalerner@umd.du)
+ * @brief Distance Alerter declaration
+ * @version 0.1
+ * @date 2021-11-24
+ * 
+ * @copyright Copyright (c) 2021. All rights reserved.
+ * This project is released under the MIT Public License.
+ * 
+ */
 
-
-#pragma once;
+#pragma once
 
 #include <ros/ros.h>
 #include <std_msgs/Empty.h>
-#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/LaserScan.h>
 
 class DistanceAlerter {
  private:
-    const double _max_dist_to_obj;
-    ros::Subscriber _cam_depth_sub;
+    const double _dist_danger_threshold;
+    ros::Subscriber _laser_scan_sub;
     ros::Publisher _danger_alerter_pub;
 
-    void pcd_callback(const sensor_msgs::PointCloud2::ConstPtr& pcd);
+    void laser_callback(const sensor_msgs::LaserScan::ConstPtr& laser_msg);
 
  public:
-    DistanceAlerter(ros::NodeHandle* nh, double max_dist=1) : _max_dist_to_obj{max_dist} {
+    DistanceAlerter(ros::NodeHandle* nh, double danger_threshold=1) : _dist_danger_threshold{danger_threshold} {
         ROS_INFO_STREAM("Spinnging up Distance Alerter.");
-        _cam_depth_sub = nh->subscribe("/realsense/depth/color/points", 1, &DistanceAlerter::pcd_callback, this);
-        _danger_alerter_pub = nh->advertise<std_msgs::Empty>("/danger", 100);
+        _laser_scan_sub = nh->subscribe("/realsense/scan", 1, &DistanceAlerter::laser_callback, this);
+        _danger_alerter_pub = nh->advertise<std_msgs::Empty>("/danger", 1);
     }
     ~DistanceAlerter() {
         ROS_INFO_STREAM("Shutting down Distance Alerter.");
--- src/distance_alerter.cpp
+++ src/distance_alerter.cpp
@@ -1,18 +1,22 @@
-
-
+/**
+ * @file distance_alerter.cpp
+ * @author Dani Lerner (dalerner@umd.du)
+ * @brief Distance Alerter executable
+ * @version 0.1
+ * @date 2021-11-24
+ * 
+ * @copyright Copyright (c) 2021. All rights reserved.
+ * This project is released under the MIT Public License.
+ * 
+ */
 
 #include <std_msgs/Empty.h>
-#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/LaserScan.h>
 #include "../include/distance_alerter.hpp"
 
-void DistanceAlerter::pcd_callback(const sensor_msgs::PointCloud2::ConstPtr& pcd) {
-    bool close = false;
-    for (const auto dist : pcd->data) {
-        if (dist < _max_dist_to_obj) {
-            close = true;
-            break;
-        }
-    }
-    if (close)
+void DistanceAlerter::laser_callback(const sensor_msgs::LaserScan::ConstPtr& laser_msg) {
+    static int halfway = laser_msg->ranges.size()/2;
+    if (laser_msg->ranges[halfway] < _dist_danger_threshold) {
         _danger_alerter_pub.publish(std_msgs::Empty());
+    }
 }
