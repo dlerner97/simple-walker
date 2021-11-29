22d9b1a1a501a425f8a8a997f5325b6fb2d8d51e
Dani Lerner <dlerner97@gmail.com>
2021-11-29 09:46:37 -0500

    Now subscribe to the danger topic. This way, any sensor can announce incoming danger and the nav stack will still try to avoid it

--- include/simple_walker.hpp
+++ include/simple_walker.hpp
@@ -10,21 +10,30 @@
  * 
  */
 
-#include <ros/ros.h>
-#include <memory>
-#include <geometry_msgs/Twist.h>
-
 #pragma once
+
+#include <memory>
+#include <ros/ros.h>
+#include <std_msgs/Empty.h>
+#include <geometry_msgs/Twist.h>
 
 class SimpleWalker {
  private:
     ros::Publisher _cmd_vel_pub;
+    ros::Subscriber _danger_sub;
+    ros::Time _prev_turn_time;
+
+    void danger_callback(const std_msgs::Empty::ConstPtr&);
+
  public:
     SimpleWalker(ros::NodeHandle* nh) {
-        _cmd_vel_pub = nh->advertise<geometry_msgs::Twist>("cmd_vel", 100);
+        _cmd_vel_pub = nh->advertise<geometry_msgs::Twist>("/cmd_vel", 100);
+        _danger_sub = nh->subscribe("/danger", 1, &SimpleWalker::danger_callback, this);
+        _prev_turn_time = ros::Time::now();
     }
     ~SimpleWalker(){}
 
     void walk(double lin_spd, double ang_spd);
+    void wander(double no_danger_rate=0.5);
 
 };
